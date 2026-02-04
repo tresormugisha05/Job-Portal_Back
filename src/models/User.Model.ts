5473b2759e511d0c01511c2405f73c61fff15345
tresor <tresormugisha07@gmail.com>
2026-02-04 16:05:20 +0200

    first version

---
+++ src/models/User.Model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcryptjs";
+export type UserRole = "Employer" | "Applicant";
+export interface UserModel extends Document{
+  FirstName: string;
+  LastName: string;
+  UserName: string;
+  Age: string;
+  PhoneNumber: string;
+  password: string;
+  profile?:string
+  UserType: UserRole;
+  resetPasswordToken?: String;
+  resetPasswordExpires?: Date;
+  createdAt: Date;
+}
+const UserSchema = new Schema<UserModel>({
+  FirstName: { type: String, required: true },
+  LastName: { type: String, required: true },
+  UserName: { type: String, required: true, unique: true },
+  Age: { type: String, required: true },
+  PhoneNumber: { type: String, required: true },
+  password: { type: String, required: true },
+  profile: { type: String },
+  UserType: { type: String, enum: ["Employer", "Applicant"], required: true },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date },
+  createdAt: { type: Date, default: Date.now }
+});
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+export default mongoose.model<UserModel>("User", UserSchema);
