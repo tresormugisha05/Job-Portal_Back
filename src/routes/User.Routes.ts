4c4b14bf7e5bd6b5575064ec893018452faed8c8
polycarpe123 <149788201+polycarpe123@users.noreply.github.com>
2026-02-12 11:20:06 +0200

    new changes

--- src/routes/User.Routes.ts
+++ src/routes/User.Routes.ts
@@ -14,14 +14,14 @@ import {
 } from "../controllers/User.Controller";
 import { getAllUsers } from "../controllers/adminController";
 const router = express.Router();
-router.post("/register", profileUpload.single("profile"), addUser)
-router.get("/", getAllUsers)
-router.post("/login", loginUser)
-router.post("/logout", protect, logoutUser)
-router.post("/change-password", protect, changePassword)
-router.get("/:id", protect, getUserById)
-router.put("/:id", protect, updateUser)
-router.delete("/:id", protect, authorize("ADMIN"), deleteUserById)
-router.patch("/:id/status", protect, authorize("ADMIN"), toggleUserStatus)
+router.post("/register", profileUpload.single("profile"), addUser);
+router.get("/", getAllUsers);
+router.post("/login", loginUser);
+router.post("/logout", protect, logoutUser);
+router.post("/change-password", protect, changePassword);
+router.get("/:id", protect, getUserById);
+router.put("/:id", protect, updateUser);
+router.delete("/:id", protect, authorize("ADMIN"), deleteUserById);
+router.patch("/:id/status", protect, authorize("ADMIN"), toggleUserStatus);
 
 export default router;
