6e131a1edae9433ff602e33412c7274898d26f2e
igihozonelly3 <igihozonelly3@gmail.com>
2026-02-14 08:55:54 +0200

    Add root route for getAllEmployers endpoint

--- src/routes/Employer.Routes.ts
+++ src/routes/Employer.Routes.ts
@@ -20,6 +20,7 @@ router.post('/register', registerEmployer);
 router.post('/login', loginEmployer);
 
 // Public route: get all employers
+router.get('/', getAllEmployers);
 router.get('/all', getAllEmployers);
 router.get('/top-hiring', getTopHiringCompanies);
 router.get('/:id', getEmployerById);
